219010c69552c2967f6760cfb697a2652c88a4e2
vijay517 <vijayganesh517@gmail.com>
2021-01-31 21:58:29 +0800

    Commit to update date and time of the system before running the batch script

--- executable_files/setup.sh
+++ executable_files/setup.sh
@@ -3,8 +3,15 @@
 # exit when any command fails
 set -e
 
+#Setting date and time
+date=$(curl -I 'https://google.com/' 2>/dev/null | grep -i '^date:' | sed 's/^[Dd]ate: //g')
+echo "Setting the date and time as"
+sudo date -s "${date}"
+
 #Setup aws cli on this edge device
+echo "Running setup_awscli bash script"
 ./setup_awscli.sh
 
 #Setup greengrass on this edge device
+echo "Running setup_greengrass bash script"
 ./setup_greengrass.sh
